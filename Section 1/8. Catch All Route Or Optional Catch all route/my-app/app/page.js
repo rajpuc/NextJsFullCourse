0966fae8ba937b624972341a9fd3b6fdf03a96a5
Rajesh Pal <ajoypal.raj@gmail.com>
2025-06-04 08:08:52 +0600

    9. Required Catch all route Or Optional catch all route

---
+++ Section 1/8. Catch All Route Or Optional Catch all route/my-app/app/page.js
@@ -0,0 +1,21 @@
+import Link from "next/link";
+
+export default async function Home({searchParams,params}) {
+
+  // http://localhost:3000/?name=Rajesh&age=25
+  console.log(await searchParams);
+  console.log(await params);
+
+  return (
+    <>
+      <h1> Welcome to my home.</h1> <br />
+      <Link href="/about">About</Link>
+      <br />
+      <Link href="/services">Services</Link>
+      <br />
+      <Link href="/blogs">Blogs</Link>
+      <br/>
+      <Link href="/files">Files</Link>
+    </>
+  );
+}
